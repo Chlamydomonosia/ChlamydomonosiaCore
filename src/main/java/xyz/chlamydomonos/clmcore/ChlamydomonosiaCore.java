a83bab0a51db3d486f7b89853aaeec34e07a5864
Chlamydomonos <2457242458@qq.com>
2021-11-17 12:43:08 +0800

    Initial commit

---
+++ src/main/java/xyz/chlamydomonos/clmcore/ChlamydomonosiaCore.java
@@ -0,0 +1,9 @@
+package xyz.chlamydomonos.clmcore;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ChlamydomonosiaCore.MODID)
+public class ChlamydomonosiaCore
+{
+    public static final String MODID = "clmcore";
+}
